ee1470934e9cf1d9026bccad008d3b9d900ad78f
Bakr Al-taie <96811292+PPFFP@users.noreply.github.com>
2022-04-15 04:13:39 +0300

    Update channelstream.py

--- handlers/channelstream.py
+++ handlers/channelstream.py
@@ -453,7 +453,7 @@ async def play(_, message: Message):
         )
         file_name = get_file_name(audio)
         title = file_name
-        thumb_name = "https://telegra.ph/file/ec11307dd8a3496d8dcbf.jpg"
+        thumb_name = "https://telegra.ph/file/385135a287245fb1414a3.jpg"
         thumbnail = thumb_name
         duration = round(audio.duration / 60)
         views = "ʟᴏᴄᴀʟʟʏ ᴀᴅᴅᴇᴅ"
